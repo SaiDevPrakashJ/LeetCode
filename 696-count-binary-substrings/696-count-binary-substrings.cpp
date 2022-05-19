9269a6348adfa968a4ae9b2503fb3a75e7a1e80c
SAI DEV PRAKASH JANAPAREDDI <91094074+SaiDevPrakashJ@users.noreply.github.com>
2022-05-19 14:16:14 -0500

    Time: 25 ms (67.25%), Space: 10.6 MB (46.76%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -3,16 +3,12 @@ public:
     int countBinarySubstrings(string s) {
         int cur=1,prev=0,res=0;
         for(int i=1;i<s.length();i++){
-            if(s[i-1]!=s[i]){
+            if(s[i-1]==s[i]){
+                cur++;
+            }else{
                 res+=min(cur,prev);
                 prev=cur;
                 cur=1;
-                //cur++;
-            }else{
-                cur++;
-                // res+=min(cur,prev);
-                // prev=cur;
-                // cur=1;
             }
         }
         return res+min(cur,prev);
