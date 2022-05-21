0c83ec228467b1959cd3f0b6d026db7d508c0d3f
SAI DEV PRAKASH JANAPAREDDI <91094074+SaiDevPrakashJ@users.noreply.github.com>
2022-05-21 12:57:58 -0500

    Time: 587 ms (44.50%), Space: 0B (100.00%) - LeetHub

--- 511-game-play-analysis-i/511-game-play-analysis-i.sql
+++ 511-game-play-analysis-i/511-game-play-analysis-i.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select player_id,event_date  as first_login from 
 (select player_id,event_date,
-dense_rank() over(partition by player_id order by event_date) as
+row_number() over(partition by player_id order by event_date) as
  ranks from Activity) A
 where ranks=1;
 
